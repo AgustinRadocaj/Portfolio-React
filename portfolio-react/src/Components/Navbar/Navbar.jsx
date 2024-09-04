313094747714df76ab485da990af60ec91b4290d
Agustin Radocaj <jaguscaj@gmail.com>
2024-09-04 09:41:08 -0300

    Final navbar

--- portfolio-react/src/Components/Navbar/Navbar.jsx
+++ portfolio-react/src/Components/Navbar/Navbar.jsx
@@ -1,20 +1,19 @@
 import React from 'react'
 import styles from './Navbar.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAddressCard, faBriefcase, faCode } from '@fortawesome/free-solid-svg-icons'
 
 export const Navbar = () => {
   return (
     <div className={styles.navbar}>
         <div className={styles.navButton}>
-            <div className={styles.navText}>Experiencia</div><img className={styles.navLogo} src="/vite.svg" alt="Experiencia"/>
+            <div className={styles.navText}>Experiencia</div><FontAwesomeIcon className={styles.icons} icon={faBriefcase} />
         </div>
         <div className={styles.navButton}>
-            <div className={styles.navText}>Proyectos</div><img className={styles.navLogo} src="/vite.svg" alt="Proyectos"/>
+            <div className={styles.navText}>Proyectos</div><FontAwesomeIcon className={styles.icons} icon={faCode} />
         </div>
         <div className={styles.navButton}>
-            <div className={styles.navText}>Certificacion</div><img className={styles.navLogo} src="/vite.svg" alt="Certificacion"/>
-        </div>
-        <div className={styles.navButton}>
-            <div className={styles.navText}>Contacto</div><img className={styles.navLogo} src="/vite.svg" alt="Contacto"/>
+            <div className={styles.navText}>Contacto</div><FontAwesomeIcon className={styles.icons} icon={faAddressCard} />
         </div>
     </div>
   )
