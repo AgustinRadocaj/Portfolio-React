7af505a851f487f8db7ba192e2e13ed6aae51de7
Agustin Radocaj <jaguscaj@gmail.com>
2024-09-05 20:08:54 -0300

    experiencia details

--- portfolio-react/src/Components/Experiencie/Experience.jsx
+++ portfolio-react/src/Components/Experiencie/Experience.jsx
@@ -1,22 +1,21 @@
 import React from 'react'
 import styles from '../Experiencie/Experience.module.css'
-import wowlogo from '../../assets/placeholder.jpg'
+import wowlogo from '../../assets/logoWow.png'
 
 export const Experience = () => {
   return (
     <>
-    <h1>Experiencia</h1>
+    <h1 className={styles.title}>Experiencia</h1>
     <div className={styles.container}>
         <div className={styles.info}>
             <h1>WOW Legal Experience</h1>
-            <h3>Full Stack Developer</h3>
+            <h3>Desarrollador Full Stack</h3>
             <p>
-                sdmnaskodbsandnbsapdnaspdnaspkdnkasdkasdnsañdkasdnkñamcsdñadasdasdasdasd
-                asdasdasdasdasd
-                sdmnaskodbsandnbsapdnaspdnaspkdnkasdkasdnsañdkasdnkñamcsdñadasdasdasdasdqqfqf
-                fqfqf
+                Diseño y desarrollo de aplicaciones web para el sector legal en poryectos internos de la empresa y 
+                clientes. Manejo de base de datos en PostgreSQL e integraciones de APIs de redes sociales de la empresa, 
+                utilizando herramientas como React, Node y Bubble.io
             </p>
-            <a href="">Linkedin</a>
+            <a  className={styles.link} target="_blank" href="https://www.linkedin.com/company/wow-legal-experience/">Linkedin</a>
         </div>
         <div className={styles.logo}>
             <img src={wowlogo} alt="wowlogo"/>
