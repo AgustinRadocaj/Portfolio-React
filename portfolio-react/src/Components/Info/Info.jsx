1acdb55aca2852c08a903992b12151102e0762e3
Agustin Radocaj <jaguscaj@gmail.com>
2024-09-05 19:41:31 -0300

    about me changes

--- portfolio-react/src/Components/Info/Info.jsx
+++ portfolio-react/src/Components/Info/Info.jsx
@@ -8,23 +8,24 @@ export const Info = () => {
         <div className={styles.title}>
             <img src={pic} alt="profile" />
             <h1>Agustin Radocaj</h1>
-            <h3>Full Stack Developer</h3>
+            <h3 className={styles.job}>Full Stack Developer</h3>
         </div>
         <div className={styles.info}>
-            <p>
+            <h3 className={styles.infoTitle}>Sobre mi</h3>
+            <p className={styles.text}>
                 Hola, soy Agustín Radocaj, un desarrollador FullStack apasionado por la tecnología y la innovación. 
                 Me gradué de la academia Henry, donde adquirí sólidas habilidades en desarrollo de aplicaciones web. 
                 Vivo en Mar del Plata, Argentina, y me dedico a seguir aprendiendo y creciendo en el ámbito del desarrollo de software.
             </p>
             <div>
-                <h3 className={styles.stackTtitle}>Stack</h3>
+                <h3 className={styles.infoTitle}>Stack</h3>
                 <div>Front-End: ReactJs - Angular - NextJs</div>
                 <div>Back-End: NodeJs - Firebase</div>
                 <div>Database: PostgreSQL</div>
                 <div>No code: Bubble</div>
                 <div>Ingles C2 Avanzado</div>
             </div>
-            <button>Descargar CV</button>
+            <button className={styles.button}>Descargar CV</button>
         </div>
     </div>
   )
