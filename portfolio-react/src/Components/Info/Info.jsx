afcbc5d65c1eab612478f4d2d77509e720cb940c
Agustin Radocaj <jaguscaj@gmail.com>
2024-09-11 16:55:54 -0300

    Update CV

--- portfolio-react/src/Components/Info/Info.jsx
+++ portfolio-react/src/Components/Info/Info.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from '../Info/Info.module.css'
 import pic from '../../assets/Profile.jpg'
-import cv from '../../assets/CV_AgustinRadocaj_FullStackDeveloper.pdf'
+import cv from  '../../assets/AgustinRadocaj_FullStack.pdf'
 
 export const Info = () => {
   return (
