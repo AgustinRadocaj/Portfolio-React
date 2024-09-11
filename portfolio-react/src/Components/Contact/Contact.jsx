b20d178dfdafa790ea088679101b76edbdb7d6e3
Agustin Radocaj <jaguscaj@gmail.com>
2024-09-11 12:42:03 -0300

    Contact final

--- portfolio-react/src/Components/Contact/Contact.jsx
+++ portfolio-react/src/Components/Contact/Contact.jsx
@@ -10,10 +10,10 @@ export const Contact = () => {
     <div className={styles.container}>
         <h1 className={styles.title}>Contacto</h1>
         <div className={styles.buttonContainer}>
-            <button className={styles.button}><FontAwesomeIcon className={styles.icon}  icon={faWhatsapp}/></button>
-            <button className={styles.button}><FontAwesomeIcon className={styles.icon}  icon={faEnvelope} /></button>
-            <button className={styles.button}><FontAwesomeIcon className={styles.icon}  icon={faLinkedinIn} /></button>
-            <button className={styles.button}><FontAwesomeIcon className={styles.icon}  icon={faGithub} /></button>
+            <a href="https://wa.me/+542995304467/" target="_blank" className={styles.link}><FontAwesomeIcon className={styles.icon}  icon={faWhatsapp}/></a>
+            <a href="mailto:jaguscaj@gmail.com" target="_blank" className={styles.link}><FontAwesomeIcon className={styles.icon}  icon={faEnvelope} /></a>
+            <a href="https://www.linkedin.com/in/agustin-radocaj/" target="_blank" className={styles.link}><FontAwesomeIcon className={styles.icon}  icon={faLinkedinIn} /></a>
+            <a href="https://github.com/AgustinRadocaj" target="_blank" className={styles.link}><FontAwesomeIcon className={styles.icon}  icon={faGithub} /></a>
         </div>
     </div>
   )
