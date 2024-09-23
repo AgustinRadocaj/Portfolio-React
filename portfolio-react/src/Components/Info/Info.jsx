c95798c3467840c0e6c92f0216a19e0a15b3bf1d
Agustin Radocaj <jaguscaj@gmail.com>
2024-09-23 15:12:27 -0300

    info language change

--- portfolio-react/src/Components/Info/Info.jsx
+++ portfolio-react/src/Components/Info/Info.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 import styles from '../Info/Info.module.css'
 import pic from '../../assets/Profile.jpg'
 import cv from  '../../assets/AgustinRadocaj_DesarrolladorFullStack.pdf'
+import { useTranslation } from 'react-i18next'
 
 export const Info = () => {
+
+    const {t} = useTranslation()
+
   return (
     <div className={styles.container}>
         <div className={styles.title}>
@@ -12,21 +16,17 @@ export const Info = () => {
             <h3 className={styles.job}>Full Stack Developer</h3>
         </div>
         <div className={styles.info}>
-            <h3 className={styles.infoTitle}>Sobre mi</h3>
-            <p className={styles.text}>
-                ¡Hola! Soy Agustín Radocaj, un desarrollador Fullstack apasionado por la tecnología y la innovación. 
-                Mi objetivo es crear aplicaciones eficientes y fáciles de usar, priorizando siempre una experiencia de usuario intuitiva
-                y fluida. Me encanta aprender constantemente para mejorar como programador y siempre estoy buscando nuevos retos para crecer tanto personal como profesionalmente.
-            </p>
+            <h3 className={styles.infoTitle}>{t('infoTitle')}</h3>
+            <p className={styles.text}>{t('infoText')}</p>
             <div>
                 <h3 className={styles.infoTitle}>Stack</h3>
                 <div>Front-End: ReactJs - Angular - NextJs</div>
                 <div>Back-End: NodeJs - Firebase</div>
                 <div>Database: PostgreSQL</div>
                 <div>No code: Bubble.io</div>
-                <div>Ingles C2 Avanzado</div>
+                <div>{t('infoLanguage')}</div>
             </div>
-            <a href={cv} target='_blank'><button className={styles.button}>Descargar CV</button></a>
+            <a href={cv} target='_blank'><button className={styles.button}>{t('CV')}</button></a>
         </div>
     </div>
   )
--- portfolio-react/src/i18n.js
+++ portfolio-react/src/i18n.js
@@ -1,5 +1,7 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
+import esTranslations from './translations/es.json'
+import enTranslations from './translations/en.json'
 
 i18n.use(initReactI18next)
 
@@ -8,28 +10,8 @@ i18n.init({
     lng: 'es',
     fallbackLng: 'en',
     resources: {
-        es: {
-            translation: {
-                "experiencia": 'experiencia',
-                "proyectos": 'proyectos',
-                "contacto": 'contacto',
-                "language": {
-                    "spanish": 'Español',
-                    "english": 'Ingles'
-                }
-            }
-        },
-        en: {
-            translation: {
-                "experiencia": 'experiencie',
-                "proyectos": 'projects',
-                "contacto": 'contact',
-                "language": {
-                    "spanish": 'Spanish',
-                    "english": 'English'
-                }
-            }
-        }
+       es: esTranslations,
+       en: enTranslations
     }
 });
 
