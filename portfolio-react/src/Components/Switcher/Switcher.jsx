63a55cb885917e21d415c91bd6fead0519ebaa16
Agustin Radocaj <jaguscaj@gmail.com>
2024-09-23 15:58:35 -0300

    Slider & styles

--- portfolio-react/src/Components/Switcher/Switcher.jsx
+++ portfolio-react/src/Components/Switcher/Switcher.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
 import i18n from '../../i18n'
+import { useState } from 'react';
+import styles from './Switcher.module.css'
 
 
-export const Switcher = ({t}) => {
+export const Switcher = ({ t }) => {
+  const [isSpanish, setIsSpanish] = useState(false);
 
-    const changeLanguage = (lng) => {
-        i18n.changeLanguage(lng);
-      };
+  const toggleLanguage = () => {
+    const newLanguage = isSpanish ? 'en' : 'es';
+    i18n.changeLanguage(newLanguage);
+    setIsSpanish(!isSpanish);
+  };
 
   return (
-    <>
-    <button onClick={()=> changeLanguage('es')}>{t('language.spanish')}</button>
-    <button onClick={()=> changeLanguage('en')}>{t('language.english')}</button>
-    </>
-  )
-}
+    <div className={styles.container}>
+    <label className={styles.switch}>
+      <input type="checkbox" checked={isSpanish} onChange={toggleLanguage} />
+      <span className={styles.slider}></span>      
+    </label>
+    <span className={styles.languageLabel}>{isSpanish ? t('language.spanish') : t('language.english')}</span>
+    </div>
+  );
+};
