cfd54fd80ee2ba3d2f023a5e1c38b9ca20218bd0
Agustin Radocaj <jaguscaj@gmail.com>
2024-09-23 15:18:13 -0300

    Experience language change

--- portfolio-react/src/Components/Experiencie/Experience.jsx
+++ portfolio-react/src/Components/Experiencie/Experience.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import styles from '../Experiencie/Experience.module.css'
 import wowlogo from '../../assets/logoWow.png'
+import { useTranslation } from 'react-i18next'
 
 export const Experience = () => {
+
+  const {t} = useTranslation()
+
   return (
     <>
-    <h1 className={styles.title}>Experiencia</h1>
+    <h1 className={styles.title}>{t('experienciaTitle')}</h1>
     <div className={styles.container}>
         <div className={styles.info}>
             <h1>WOW Legal Experience</h1>
-            <h3>Desarrollador Full Stack</h3>
-            <p>
-                Diseño y desarrollo de aplicaciones web para el sector legal en poryectos internos de la empresa y 
-                clientes. Manejo de base de datos en PostgreSQL e integraciones de APIs de redes sociales de la empresa, 
-                utilizando herramientas como React, Node y Bubble.io
-            </p>
+            <h3>{t('job')}</h3>
+            <p>{t('jobDescription')}</p>
             <a  className={styles.link} target="_blank" href="https://www.linkedin.com/company/wow-legal-experience/">Linkedin</a>
         </div>
         <div className={styles.logo}>
